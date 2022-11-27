0bcf3fb1b178ea4e9a54214949994599fb23642d
David <davidtonyawusaku@gmail.com>
2022-11-27 01:26:16 +0100

    Node set 2

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
 
 const app = express();
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:'',
+    database:"moviebook",
+});
 
-app.get("/",(req,res) =>{
-    res.send("<h1>Home Page</h1>")
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+db.connect( (error) =>{
+    if(error){
+        console.log(error);
+    } else{
+        console.log("Mysql connected");
+    }
 })
+
+// ------------------------------------------------------------
+
+//Routes
+app.get("/",(req,res) =>{
+    res.render("index");
+});
 
 
 
